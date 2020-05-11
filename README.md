14ae3a4a470745fc179273f206d025d43f926885
Gesare <gesareayienda@gmail.com>
2020-05-11 10:11:21 +0300

    Added a picture

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 
 ### By **Gesare Ombati**
 
-
 ## Link to Live Site 
-
 
 ## Description
   The application allows us to create a personal blogging website where you can create and share your opinions and other users can read and comment on them. Additionally, add a feature that displays random quotes to inspire your users. 
--- app/auth/forms.py
+++ app/auth/forms.py
@@ -14,7 +14,7 @@ class RegistrationForm(FlaskForm):
 
     def validate_email(self, data_field):
         if User.query.filter_by(email=data_field.data).first():
-            raise ValidationError('There is an account with that email')
+            raise ValidationError('This username has been taken.')
 
     def validate_username(self, data_field):
         if User.query.filter_by(username=data_field.data).first():
